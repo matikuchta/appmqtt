2536dff88c29a2b5ee70699123e0a439fbd31822
matikuchta <mateusz.kuchta@solarisbus.com>
2025-05-14 13:50:56 +0200

    create api.py file

--- src/__main__.py
+++ src/__main__.py
@@ -4,7 +4,7 @@ import signal
 import json
 import logging as log
 from typing import Any
-from flask import Flask, request, jsonify
+
 
 from app.mqttClient import MQTTClient
 from app.personFasada import PersonFasada
@@ -12,12 +12,15 @@ from app.types.person import Person
 from app.types.config import Config
 from app.types.updatepersondata import UpdatePersonData
 from app.types.getdelpersondata import GetDelPersonData
+from app.api import API
+from flask import Flask, request, jsonify
 
 
 log.basicConfig(level=log.DEBUG)
 
 def handleExit(signal: int, frame: Any)->None:
     log.info("The app has been terminated successfully")
+    sub.SaveData()
     sys.exit(0)
 
 
@@ -48,63 +51,7 @@ if __name__ == '__main__':
     client.connect()
     client.subscribe("app/+/+/request")
     client.client.loop_start()
-    '''FLASK'''
     app = Flask(__name__)
-    @app.route('/')
-    def root():
-        return "root"
-    @app.get('/person')
-    def get_person():
-        pesel = request.args.get('pesel')
-        data:GetDelPersonData = {'pesel': pesel}
-        res = sub.GetPerson(data)
-        log.debug(res)
-        return jsonify(res)
-    @app.get('/persons')
-    def get_persons():
-        res = sub.GetPersons()
-        log.debug(res)
-        return jsonify(res)
-    @app.post('/person')
-    def add_person():
-        person:Person = {
-            "imie" : request.args.get('imie'),
-            "nazwisko" : request.args.get('nazwisko'),
-            "pesel" : request.args.get('pesel'),
-            "stanowisko" : request.args.get('stanowisko'),
-            "data_urodzenia" : request.args.get('data_urodzenia'),
-            "data_zatrudnienia" : request.args.get('data_zatrudnienia')
-        }
-        sub.AddPerson(json.dumps(person))
-        sub.SaveData()
-        return jsonify({})
-    @app.delete('/person')
-    def del_person():
-        pesel = request.args.get('pesel')
-        data:GetDelPersonData = {'pesel': pesel}
-        sub.RemovePerson(json.dumps(data))
-        sub.SaveData()
-        return jsonify({})
-    @app.patch('/person')
-    def patch_person():
-        person_data:Person = {}
-        if request.args.get('imie'):
-            person_data["imie"] = request.args.get('imie')
-        if request.args.get('nazwisko'):
-            person_data["nazwisko"] = request.args.get('nazwisko')
-        if request.args.get('stanowisko'):
-            person_data["stanowisko"] = request.args.get('stanowisko')
-        if request.args.get('data_urodzenia'):
-            person_data["data_urodzenia"] = request.args.get('data_urodzenia')
-        if request.args.get('data_zatrudnienia'):
-            person_data["data_zatrudnienia"] = request.args.get('data_zatrudnienia')
-        print(json.dumps(person_data))
-        data:UpdatePersonData = {
-            "pesel" : request.args.get('pesel'),
-            "to_update" : person_data
-        }
-        sub.ModifyPerson(json.dumps(data))
-        sub.SaveData()
-        return jsonify({})
+    api = API(app, sub)
     app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False)
-    '''FLASK'''
+
---
+++ src/app/api.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+import logging as log
+import json
+
+from app.types.person import Person
+from app.types.config import Config
+from app.personFasada import PersonFasada
+from app.types.updatepersondata import UpdatePersonData
+from app.types.getdelpersondata import GetDelPersonData
+class API:
+    def __init__(self, app:Flask, sub:PersonFasada):
+        self.sub:PersonFasada = sub
+        app.add_url_rule('/', 'root', self.root)
+        app.add_url_rule('/person', 'get_person', self.get_person, methods=['GET'])
+        app.add_url_rule('/persons', 'get_persons', self.get_persons, methods=['GET'])
+        app.add_url_rule('/person', 'add_person', self.add_person, methods=['POST'])
+        app.add_url_rule('/person', 'del_person', self.del_person, methods=['DELETE'])
+        app.add_url_rule('/person', 'patch_person', self.patch_person, methods=['PATCH'])
+    def root(self):
+        return "root"
+    def get_person(self):
+        pesel = request.args.get('pesel')
+        data:GetDelPersonData = {'pesel': pesel}
+        res = self.sub.GetPerson(data)
+        log.debug(res)
+        return jsonify(res)
+    def get_persons(self):
+        res = self.sub.GetPersons()
+        log.debug(res)
+        return jsonify(res)
+    def add_person(self):
+        person:Person = {
+            "imie" : request.args.get('imie'),
+            "nazwisko" : request.args.get('nazwisko'),
+            "pesel" : request.args.get('pesel'),
+            "stanowisko" : request.args.get('stanowisko'),
+            "data_urodzenia" : request.args.get('data_urodzenia'),
+            "data_zatrudnienia" : request.args.get('data_zatrudnienia')
+        }
+        self.sub.AddPerson(json.dumps(person))
+        self.sub.SaveData()
+        return jsonify({})
+    def del_person(self):
+        pesel = request.args.get('pesel')
+        data:GetDelPersonData = {'pesel': pesel}
+        self.sub.RemovePerson(json.dumps(data))
+        self.sub.SaveData()
+        return jsonify({})
+    def patch_person(self):
+        person_data:Person = {}
+        if request.args.get('imie'):
+            person_data["imie"] = request.args.get('imie')
+        if request.args.get('nazwisko'):
+            person_data["nazwisko"] = request.args.get('nazwisko')
+        if request.args.get('stanowisko'):
+            person_data["stanowisko"] = request.args.get('stanowisko')
+        if request.args.get('data_urodzenia'):
+            person_data["data_urodzenia"] = request.args.get('data_urodzenia')
+        if request.args.get('data_zatrudnienia'):
+            person_data["data_zatrudnienia"] = request.args.get('data_zatrudnienia')
+        print(json.dumps(person_data))
+        data:UpdatePersonData = {
+            "pesel" : request.args.get('pesel'),
+            "to_update" : person_data
+        }
+        self.sub.ModifyPerson(json.dumps(data))
+        self.sub.SaveData()
+        return jsonify({})
+    
