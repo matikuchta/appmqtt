90bb4ddcc9141a386f85c880f0fd820ac678bdb8
matikuchta <mateusz.kuchta@solarisbus.com>
2025-05-23 11:16:27 +0200

    add django data validation, date of creation and modification auto setting, js confirmation of deleting a person

--- src/personsapi/persons/models.py
+++ src/personsapi/persons/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Job(models.Model):
-    nazwa = models.CharField(max_length=100)
+    nazwa = models.CharField(max_length=100, unique=True)
 class Person(models.Model):
     imie = models.CharField(max_length=100)
     nazwisko = models.CharField(max_length=100)
@@ -8,6 +8,6 @@ class Person(models.Model):
     stanowisko = models.ForeignKey(Job, on_delete=models.CASCADE, related_name="stanowisko")
     data_urodzenia = models.DateField()
     data_zatrudnienia = models.DateField()
-    data_modyfikacji = models.DateField()
-    data_utworzenia = models.DateField()
+    data_modyfikacji = models.DateTimeField(auto_now=True)
+    data_utworzenia = models.DateTimeField(auto_now_add=True)
 
--- src/personsapi/persons/serializers.py
+++ src/personsapi/persons/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import Person, Job
+import re
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = ['id', 'nazwa']
+    def validate_nazwa(self, value):
+        if (not re.search("^[A-Za-z]{1,}$", value)):
+            raise serializers.ValidationError("job title must at least 2 characters long, and contain only letters")
+        return value
 
 
 # Serializer dla osoby (Person)
@@ -18,3 +23,18 @@ class PersonSerializer(serializers.ModelSerializer):
             'data_urodzenia', 'data_zatrudnienia', 
             'data_modyfikacji', 'data_utworzenia'
         ]
+        read_only_fields = ['data_utworzenia', 'data_modyfikacji']
+    def validate_pesel(self, value):
+        if (len(str(value))!=11):
+            raise serializers.ValidationError("pesel must be 11 digit long")
+        return value
+    def validate_imie(self, value):
+        if (not re.search("^[A-Za-z]{1}[a-z]{2,}$", value)):
+            raise serializers.ValidationError("name must at least 3 characters long, contain only lowercase letters except the first character which can be uppercase")
+        return value
+    def validate_nazwisko(self, value):
+        if (not re.search("^[A-Za-z]{1}[a-z]{1,}$", value)):
+            raise serializers.ValidationError("surname must at least 2 characters long, contain only lowercase letters except the first character which can be uppercase")
+        return value
+
+
